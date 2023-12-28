61bad36bc843157b573831e26f658dd45552cfe8
AndresBorda24 <anjart24@gmail.com>
2023-12-28 08:03:12 -0500

    Ajuste Auth-Middleware

--- src/lib/router.js
+++ src/lib/router.js
@@ -1,4 +1,5 @@
 import { routes } from "@/routes"
+import { storeToRefs } from "pinia"
 import { useAuthStore } from "@/stores/Auth"
 import { useViewLoad } from "@/stores/ViewLoad"
 import { createRouter, createWebHistory } from 'vue-router'
@@ -12,16 +13,18 @@ export function getRouter() {
 
     router.beforeEach(async (to) => {
         viewLoad.setLoadingTrue()
+        await useAuthStore().session()
+        const { state: auth, isLogged } = storeToRefs( useAuthStore() )
+
         if (to.matched.some(record => record.meta.requiresAuth)) {
-            if (! await useAuthStore().session())
-                return { name: "unauthorized" }
+            if (! isLogged.value) return { name: "unauthorized" }
         }
 
         const requiredArea = to.matched.some(record => {
             const areas = record.meta.requiresAreas
-            if (! areas || ! Array.isArray(areas)) return false
 
-            if (! areas.includes(useAuthStore().state.area)) return true
+            if (! areas || ! Array.isArray(areas)) return false
+            if (! areas.includes(auth.value.area)) return true
 
             return false
         })
